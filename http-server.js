96625c52b60a882ce4a5485bd6d8529dac592f19
HM-Tanjil9 <rtanjil0@gmail.com>
2025-06-24 23:30:34 +0600

    nodejs updated

--- http-server.js
+++ http-server.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("New connection was created");
+    res.end("Something gg");
 })
 
 server.listen(3000, () => {
